bf1dd39f158be8065df597b624e1cf76dd9a9299
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2022-12-22 23:25:20 -0500

    Fix [length] not accepting advanced expression

--- shortcodes/basic/length.py
+++ shortcodes/basic/length.py
@@ -6,5 +6,5 @@ class Shortcode():
 	def run_atomic(self, pargs, kwargs, context):
 		_delimiter = self.Unprompted.parse_advanced(kwargs["_delimiter"],context) if "_delimiter" in kwargs else self.Unprompted.Config.syntax.delimiter
 		_max = self.Unprompted.parse_advanced(kwargs["_max"],context) if "_max" in kwargs else -1
-		strings = pargs[0].split(_delimiter,_max)
+		strings = self.Unprompted.parse_advanced(pargs[0],context).split(_delimiter,_max)
 		return(len(strings))
