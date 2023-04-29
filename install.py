0e87170b6522def93b4ead21e984039ed058493d
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2023-04-28 20:51:26 -0400

    Attempted requirements workaround for Linux

--- install.py
+++ install.py
@@ -15,7 +15,11 @@ def migrate_folder(old_dir,new_dir):
 if not launch.is_installed("nltk"):
     launch.run_pip("install nltk", "requirements for Unprompted")
 if not launch.is_installed("pattern"):
-	launch.run_pip("install pattern@git+https://github.com/NicolasBizzozzero/pattern","requirements for Unprompted")
+	try:
+		launch.run_pip("install pattern@git+https://github.com/NicolasBizzozzero/pattern","requirements for Unprompted")
+	except Exception as e:
+		print(f"Could not install pattern - some shortcodes may not work as expected: {e}")
+		pass
 if not launch.is_installed("sentence-transformers"):
 	launch.run_pip("install sentence-transformers","requirements for Unprompted - img2pez")
 if not launch.is_installed("salesforce-lavis"):
